b04bdfb75c7cfb00854640c1d385b72e9c34d56a
siubacy <58424019+siubacy@users.noreply.github.com>
2022-08-23 16:07:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
+  print(weather['temp'])
   return weather['weather'], math.floor(weather['temp'])
 
 def get_count():
