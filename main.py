b8fa755f81713dc4afcabe580b05cc45a80abb78
siubacy <58424019+siubacy@users.noreply.github.com>
2022-08-23 16:58:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_weather():
   print(res)
   weather = res['data']['list'][0]
   
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temp']),weather['date']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -49,7 +49,7 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature,mytime = get_weather()
+data = {"time":{"value":mytime},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
